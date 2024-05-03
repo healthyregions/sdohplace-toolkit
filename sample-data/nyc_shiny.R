865100390a35cc27d67ab1105239fc3d1974f0b6
Marynia <marynia.kolak@gmail.com>
2024-05-02 20:58:11 -0500

    make rshiny pretty

--- sample-data/nyc_shiny.R
+++ sample-data/nyc_shiny.R
@@ -1,35 +1,52 @@
 # Load necessary libraries
 library(shiny)
+library(shinythemes)
 library(leaflet)
 library(sf)
 library(plotly)
-library(dplyr)
+library(tidyverse)
 
 # Load and prepare the data
 nyc_data <- st_read("NYC_nbrhd_data.geojson", quiet = TRUE)
 nyc_data <- st_make_valid(nyc_data)
 nyc_data <- st_transform(nyc_data, crs = 4326)
+nyc_data_df <- st_drop_geometry(nyc_data)
 
 # Define UI for the first tab
 tab1_ui <- tabPanel("Self-Identified Race & Ethnicty",
                     sidebarLayout(
                       sidebarPanel(
                         p("Select different variables from the dropdown menus to explore the data."),
-                        selectInput("color", "Self-Identified Race & Ethnicity:",
-                                    choices = c("Percent Black" = "pctblack",
+                        selectInput("race", "Self-Identified Race & Ethnicity:",
+                                    choices = c("Percent Asian & Pacific Islander" = "pctapi",
+                                                "Percent Black" = "pctblack",
                                                 "Percent Hispanic" = "pcthisp",
-                                                "Percent White" = "pctwhite"),
+                                                "Percent White" = "pctwhite",
+                                                "Percent Other Identified Race" = "pctother"),
                                     selected = "pctblack"),
                      
                         selectInput("neighborhood", "NYC Neighborhood:",
-                                    choices = nyc_data$NTAName,
+                                    choices = str_sort(nyc_data$NTAName),
                                     selected = "Pelham Bay-Country Club-City Island"),
                         
                         helpText("Data source: NYC Neighborhood Data"),
+                        
                         br(),
-                        h3("About This App"),
-                        p("This application provides a visual representation of demographic and health data across New York City's neighborhoods."),
-                      ),
+                        
+                        h3("Racial & Ethnic Disparities"),
+                        
+                        p("Extensive research has shown that racial and ethnic disparities in 
+                        quality of care and use of services exist and persist in the United States.
+                        Disparities may emerge from unequal access to health care, critical resources
+                        such as health foods, housing, and transportation."),
+                        br(),
+                        p("Explore racial and ethnic population distributions by NYC neighborhood in this tab,
+                        and then explore socioeconomic and health trends acrosos the rest of the applications.
+                        Identify locations for further analysis."),
+                        
+                        helpText("Read More: The Commonwealth Fund 2024 State Health Disparities Report")
+                        ),
+                        
                       mainPanel(
                         fluidRow(leafletOutput("map"),
                                  br(),
@@ -41,21 +58,29 @@ tab1_ui <- tabPanel("Self-Identified Race & Ethnicty",
 
 # Define server logic for the first tab
 tab1_server <- function(input, output, session) {
+  
   # Map output for Racial Demographics
   output$map <- renderLeaflet({
-    map_data <- st_transform(nyc_data, crs = 4326)
     
-    valid_data <- map_data[!is.na(map_data[[input$color]]), ]
-    pal <- colorQuantile("PuBuGn", valid_data[[input$color]], n = 5)
+    valid_data <- nyc_data[!is.na(nyc_data[[input$race]]), ]
+    pal <- colorQuantile("viridis", valid_data[[input$race]], n = 5)
     
     leaflet(valid_data) %>%
       addProviderTiles(providers$CartoDB.Positron) %>%
       addPolygons(
-        fillColor = ~pal(valid_data[[input$color]]),
-        fillOpacity = 0.7, weight = 0.8, color = "white",
+        fillColor = ~pal(valid_data[[input$race]]),
+        fillOpacity = 0.7, weight = 1, color = "white",
         popup = ~paste(NTAName, "<br>",
-                       paste(input$color, ":", round(valid_data[[input$color]], 2), "%"))
+                       paste(input$race, ":", round(valid_data[[input$race]], 2), "%"))
       ) %>%
+      
+      addLegend(
+        position = "bottomright",
+        pal = pal,
+        values = valid_data[[input$race]],
+        title = "% of population"
+      )  %>%
+      
       setView(lng = -73.935242, lat = 40.730610, zoom = 10)
   })
   
@@ -63,10 +88,9 @@ tab1_server <- function(input, output, session) {
   output$racialDemoChart <- renderPlotly({
     chart_data <- nyc_data[nyc_data$NTAName == input$neighborhood, ]
     
-    # Lose the spatial component for the chart data
+    # Extract data and remove "geometry" column
     racial_data <- st_drop_geometry(chart_data)
     
-    # Subselect to only variables we're interested in
     racial_data <- racial_data[, c("pctblack", "pcthisp", "pctwhite", "pctapi", "pctother")]
     
     racial_data <- t(racial_data)
@@ -75,11 +99,12 @@ tab1_server <- function(input, output, session) {
     rownames(racial_data) <- NULL
     
     plot_ly(data = as.data.frame(racial_data), x = ~Race, y = ~Percentage, type = 'bar', color = ~Race) %>%
-      layout(title = paste("Self-Identified Race -", input$neighborhood),
+      layout(title = paste("Racial Demographics -", input$neighborhood),
              xaxis = list(title = "Race"),
              yaxis = list(title = "Percentage", range = c(0, 100), tickvals = seq(0, 100, 20)))
   })
 }
+
 
 # Define UI for the second tab
 tab2_ui <- tabPanel("Socioeconomic Demographics",
@@ -91,12 +116,18 @@ tab2_ui <- tabPanel("Socioeconomic Demographics",
                                                 "Rent < 50% of Income" = "rent.50"),
                                     selected = "pctpov"),
                         selectInput("neighborhood_socio", "Select Neighborhood:",
-                                    choices = nyc_data$NTAName,
+                                    choices = str_sort(nyc_data$NTAName),
                                     selected = "Pelham Bay-Country Club-City Island"),
+                        
                         helpText("Data source: NYC Neighborhood Data"),
+                        
                         br(),
-                        h3("About This App"),
-                        p("This application provides a visual representation of demographic and health data across New York City's neighborhoods."),
+                        
+                        h3("Socioeconomic Disparities"),
+                        
+                        p("Nulla suscipit, purus ac varius sagittis, velit lorem condimentum ipsum, sit amet auctor sem tellus a leo. Aenean faucibus hendrerit diam non rutrum. Proin nec nisi dolor. Nam egestas dolor sapien, eget pellentesque neque tincidunt nec. Phasellus mattis pulvinar tincidunt. Phasellus eget condimentum nisl. Praesent dapibus dui elit, id fringilla quam interdum vel. Praesent vestibulum nulla et rutrum ornare. Donec cursus felis dui, et auctor nisi pulvinar ac. Suspendisse placerat ex sed arcu semper volutpat. Donec commodo consequat ornare. Aenean est lectus, semper at luctus sit amet, bibendum vitae augue. Donec risus felis, commodo eget tristique vitae, imperdiet in risus."),
+                        
+                        helpText("Read More: Include text here ")
                       ),
                       mainPanel(
                         fluidRow(leafletOutput("map_socio"),
@@ -107,14 +138,14 @@ tab2_ui <- tabPanel("Socioeconomic Demographics",
                     )
 )
 
-# Define server logic for the second tab
+# Define server for the second tab
 tab2_server <- function(input, output, session) {
+  
   # Map output for Socioeconomic Demographics
   output$map_socio <- renderLeaflet({
-    map_data <- st_transform(nyc_data, crs = 4326)
     
-    valid_data <- map_data[!is.na(map_data[[input$color_socio]]), ]
-    pal <- colorQuantile("PuBuGn", valid_data[[input$color_socio]], n = 5)
+    valid_data <- nyc_data[!is.na(nyc_data[[input$color_socio]]), ]
+    pal <- colorQuantile("viridis", valid_data[[input$color_socio]], n = 5)
     
     leaflet(valid_data) %>%
       addProviderTiles(providers$CartoDB.Positron) %>%
@@ -124,16 +155,23 @@ tab2_server <- function(input, output, session) {
         popup = ~paste(NTAName, "<br>",
                        paste(input$color_socio, ":", round(valid_data[[input$color_socio]], 2), "%"))
       ) %>%
+      
+      addLegend(
+        position = "bottomright",
+        pal = pal,
+        values = valid_data[[input$color_socio]],
+        title = "% of population"
+      )  %>%
+      
       setView(lng = -73.935242, lat = 40.730610, zoom = 10)
   })
   
   # Socioeconomic Demographics chart
   output$socioDemoChart <- renderPlotly({
-    chart_data <- nyc_data[nyc_data$NTAName == input$neighborhood_socio, ]
+    chart_data <- nyc_data_df[nyc_data_df$NTAName == input$neighborhood_socio, ]
     
     # Extract data and remove "geometry" column
-    socio_data <- as.data.frame(chart_data)
-    socio_data <- socio_data[, -which(names(socio_data) == "geometry")]
+    socio_data <- st_drop_geometry(chart_data)
     
     socio_data <- socio_data[, c("pctpov", "rent.30", "rent.50")]
     
@@ -149,6 +187,7 @@ tab2_server <- function(input, output, session) {
   })
 }
 
+
 # Define UI for the third tab
 tab3_ui <- tabPanel("Severe Maternal Morbidity & Preterm Birth Rates",
                     sidebarLayout(
@@ -157,10 +196,16 @@ tab3_ui <- tabPanel("Severe Maternal Morbidity & Preterm Birth Rates",
                                     choices = c("Severe Maternal Morbidity Rate" = "smmrate",
                                                 "Preterm Birth Rate" = "ptbrate"),
                                     selected = "smmrate"),
+                        
                         helpText("Data source: NYC Neighborhood Data"),
+                        
                         br(),
-                        h3("About This App"),
-                        p("This application provides a visual representation of demographic and health data across New York City's neighborhoods."),
+                        
+                        h3("Maternal Health Outcomes"),
+                        
+                        p("Pellentesque nisl ipsum, bibendum non porttitor eget, lobortis sit amet arcu. Aliquam et erat nec nisi fermentum aliquet non a massa. Mauris vel sapien justo. Sed fermentum sed purus ut fringilla. Aliquam pulvinar, ligula ac ornare rutrum, est ipsum tristique metus, non imperdiet nibh ligula id elit. Proin ac dui in ligula finibus facilisis. Quisque at vulputate nulla, sit amet varius nunc. In eu cursus quam. In diam est, tristique sit amet nunc nec, vehicula hendrerit odio. Phasellus est turpis, vulputate eu suscipit sit amet, semper at enim. Vivamus sit amet risus leo. Vestibulum porttitor feugiat ipsum, ut volutpat erat pharetra quis. Suspendisse interdum ultrices nisi vel finibus. Aliquam lobortis sed arcu eget ornare."),
+                        
+                        helpText("Read More: Include text here ")
                       ),
                       mainPanel(
                         fluidRow(leafletOutput("map_health"),
@@ -173,12 +218,13 @@ tab3_ui <- tabPanel("Severe Maternal Morbidity & Preterm Birth Rates",
 
 # Define server logic for the third tab
 tab3_server <- function(input, output, session) {
+  
   # Map output for Health Demographics
+  
   output$map_health <- renderLeaflet({
-    map_data <- st_transform(nyc_data, crs = 4326)
     
-    valid_data <- map_data[!is.na(map_data[[input$color_health]]), ]
-    pal <- colorQuantile("PuBuGn", valid_data[[input$color_health]], n = 5)
+    valid_data <- nyc_data[!is.na(nyc_data[[input$color_health]]), ]
+    pal <- colorBin("viridis", valid_data[[input$color_health]], pretty = FALSE, n = 5)
     
     leaflet(valid_data) %>%
       addProviderTiles(providers$CartoDB.Positron) %>%
@@ -188,31 +234,88 @@ tab3_server <- function(input, output, session) {
         popup = ~paste(NTAName, "<br>",
                        paste(input$color_health, ":", round(valid_data[[input$color_health]], 2)))
       ) %>%
+      
+      addLegend(
+        position = "bottomright",
+        pal = pal,
+        values = valid_data[[input$color_health]],
+        title = "Rate per X,000 persons"
+      )  %>%
+      
       setView(lng = -73.935242, lat = 40.730610, zoom = 10)
   })
   
   # Health Demographics scatter plot
+  
   output$healthScatterChart <- renderPlotly({
-    plot_ly(nyc_data, x = ~smmrate, y = ~ptbrate, text = ~NTAName, mode = 'markers') %>%
+    
+    plot_ly(nyc_data_df, x = ~smmrate, y = ~ptbrate, text = ~NTAName, type = 'scatter') %>%
       layout(title = "Severe Maternal Morbidity vs Preterm Birth Rates",
-             xaxis = list(title = "Severe Maternal Morbidity Rate"),
-             yaxis = list(title = "Preterm Birth Rate", range = c(0, 15)))
+             xaxis = list(title = "Severe Maternal Morbidity Rate", zeroline = TRUE),
+             yaxis = list(title = "Preterm Birth Rate", zeroline = TRUE))
   })
 }
 
 
-
 # Define UI for the fourth tab
 tab4_ui <- tabPanel("About",
-                    h3("About This App"),
-                    p("This application provides a visual representation of demographic and health data across New York City's neighborhoods. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisi porta lorem mollis aliquam ut. Dui id ornare arcu odio ut sem nulla pharetra diam. Velit dignissim sodales ut eu sem integer vitae justo eget. Diam donec adipiscing tristique risus nec feugiat in fermentum. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Eget dolor morbi non arcu risus. Eget mauris pharetra et ultrices neque ornare aenean. Sed vulputate odio ut enim blandit volutpat maecenas volutpat. Laoreet non curabitur gravida arcu ac tortor dignissim. Blandit massa enim nec dui. Adipiscing bibendum est ultricies integer quis. Sagittis vitae et leo duis ut. Id interdum velit laoreet id."),
-                    p("Select different demographic and health variables from the dropdown menus to explore the data. Nulla pellentesque dignissim enim sit amet venenatis urna cursus. Eget gravida cum sociis natoque penatibus. Feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi. Mi ipsum faucibus vitae aliquet nec ullamcorper sit. Sit amet consectetur adipiscing elit. Urna id volutpat lacus laoreet non curabitur gravida arcu ac. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Nibh sit amet commodo nulla. Porta lorem mollis aliquam ut porttitor leo a. Arcu odio ut sem nulla. Orci a scelerisque purus semper eget duis at. Mi sit amet mauris commodo quis imperdiet massa. Sem et tortor consequat id. Consectetur libero id faucibus nisl. Pellentesque habitant morbi tristique senectus et netus et malesuada fames. Turpis nunc eget lorem dolor sed. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra orci. Lacus laoreet non curabitur gravida arcu ac tortor dignissim."),
-                    p("Data source: NYC Neighborhood Data, provided in GeoJSON format and visualized using Leaflet and Plotly in a Shiny application.")
-)
+                    sidebarLayout( 
+                      sidebarPanel(
+                        
+                        h3("Data"),
+                        
+                        p("Mauris vel sapien justo. Sed fermentum sed purus ut fringilla. Aliquam pulvinar, 
+                          ligula ac ornare rutrum, est ipsum tristique metus, non imperdiet nibh ligula id 
+                          elit."), 
+                                                           br(),
+                          
+                        
+                        h3("Methodology"),
+                        
+                        p("Proin ac dui in ligula finibus facilisis. Quisque at vulputate nulla, sit
+                          amet varius nunc. In eu cursus quam. In diam est, tristique sit amet nunc nec, 
+                          vehicula hendrerit odio. "),
+                        
+                        helpText("Read More: Include text here ")
+                      ),
+                      mainPanel(
+                        fluidRow( 
+                        
+                          h2("Motivations & Background"),
+                          p("Phasellus est turpis, vulputate eu suscipit sit amet, 
+                          semper at enim. Vivamus sit amet risus leo. Vestibulum porttitor feugiat ipsum, 
+                          ut volutpat erat pharetra quis. Suspendisse interdum ultrices nisi vel finibus. 
+                          Aliquam lobortis sed arcu eget ornare."),
+                                
+                                br(),
+                        
+                          h2("Study Findings"),
+                          p("Phasellus est turpis, vulputate eu suscipit sit amet, 
+                            semper at enim. Vivamus sit amet risus leo. Vestibulum porttitor feugiat ipsum, 
+                            ut volutpat erat pharetra quis. Suspendisse interdum ultrices nisi vel finibus. 
+                            Aliquam lobortis sed arcu eget ornare."),
+                        
+                        br(),
+                        
+                        h2("Team"),
+                        p("Phasellus est turpis, vulputate eu suscipit sit amet, 
+                          semper at enim. Vivamus sit amet risus leo. Vestibulum porttitor feugiat ipsum, 
+                          ut volutpat erat pharetra quis. Suspendisse interdum ultrices nisi vel finibus. 
+                          Aliquam lobortis sed arcu eget ornare."),
+                        
+                        br(),
+                        
+                        h2("Questions? Contact Us."),
+                        p("Email person@person.com for more information."),
+                        
+                        ))))
+
+                    
 
 # Define UI for the app
 ui <- fluidPage(
-  titlePanel("NYC Neighborhood Demographics"),
+  theme = shinytheme("yeti"),
+  titlePanel("NYC Neighborhood & Health Demographics"),
   tabsetPanel(
     tab1_ui,
     tab2_ui,
